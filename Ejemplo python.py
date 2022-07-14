5675e8f4bf895a68a55d872e85bf8cc27fdb595f
Verosmc99 <veronica.martinez@ute.edu.ec>
2022-07-13 22:06:25 -0500

    first commit

---
+++ Ejemplo python.py
@@ -0,0 +1,23 @@
+"""
+ _______       _            _     _          ______        _                 _ 
+(_______)     (_)       _  (_)   | |        (____  \      (_)               | |
+ _______  ____ _  ___ _| |_ _  __| |_____    ____)  ) ____ _ _____ ____   __| |
+|  ___  |/ ___) |/___|_   _) |/ _  | ___ |  |  __  ( / ___) (____ |  _ \ / _  |
+| |   | | |   | |___ | | |_| ( (_| | ____|  | |__)  ) |   | / ___ | | | ( (_| |
+|_|   |_|_|   |_(___/   \__)_|\____|_____)  |______/|_|   |_\_____|_| |_|\____|
+    
+Auteur: 59399(59399.pro@gmail.com) 
+Untitled-1(Ɔ) 2022
+Description : Saisissez la description puis « Tab »
+Créé le :  mercredi 13 juillet 2022, 22:02:28 
+Dernière modification : mercredi 13 juillet 2022, 22:04:40
+"""
+
+#Calcular el promedio de tres notas.
+print("MOSTRAR EL PROMEDIO DE 3 NOTAS")
+nota1 = int(input("Ingrese Nota 01 : "))
+nota2 = int(input("Ingrese Nota 02 : "))
+nota3 = int(input("Ingrese Nota 03 : "))
+print()
+prom = (nota1+nota2+nota3)/3
+print("EL PROMEDIO ES : ",prom)
